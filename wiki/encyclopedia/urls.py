156c4ce7d7dc8d603c90e4ce384de2097bab08d1
bugradursun <72981010+bugradursun@users.noreply.github.com>
2023-12-01 14:36:46 +0300

    a

--- wiki/encyclopedia/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path("", views.index, name="index"), #homepagede index function goruntuulenecek (path,function we want to call,optinoally name for the path)
-    path("new/",views.new,name="new"),
-
-]
