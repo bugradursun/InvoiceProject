4bdad34880c05e9bffa5760697825f8183c89725
bugradursun <72981010+bugradursun@users.noreply.github.com>
2023-12-06 10:48:48 +0300

    context

--- multisigwallet-frontend/src/contexts/MultiSigWallet.tsx
+++ multisigwallet-frontend/src/contexts/MultiSigWallet.tsx
@@ -3,3 +3,42 @@ import BN from "bn.js";
 import React,{useReducer,useEffect,createContext,useContext,useMemo} from "react";
 import { useWeb3Context } from "./Web3";
 import {get as getMultiSigWallet,subscribe} from "../api/multi-sig-wallet";
+
+//will get data from api/multi-sig-wallet.ts and render here
+
+interface State {
+    address: string;
+    balance:string;
+    owners:string[];
+    numConfirmationsRequired:number;
+    transactionCount:number;
+    transactions:Transaction[]
+}
+
+interface Transaction {
+    txIndex:number;
+    to:string;
+    value:BN;
+    data:string;
+    executed:boolean;
+    numConfirmations:number;
+    isConfirmedByCurrentAccount:boolean;
+}
+
+const INITIAL_STATE : State = {
+    address:"",
+    balance:"0",
+    owners:[],
+    numConfirmationsRequired:0,
+    transactionCount:0,
+    transactions:[],
+}
+
+//actions
+const SET = "SET"
+const UPDATE_BALANCE = "UPDATE_BALANCE"
+const ADD_TX = "ADD_TX"
+const UPDATE_TX = "UPDATE_TX"
+
+
+
